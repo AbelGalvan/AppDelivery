3007602a858967ccb049536db64b92763f7f85f8
AbelGalvan <37642705+AbelGalvan@users.noreply.github.com>
2020-08-10 22:55:36 -0600

    Inicio App

---
+++ AppSDelivery/AppSDelivery/AppSDelivery/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppSDelivery.Services;
+using AppSDelivery.Views;
+
+namespace AppSDelivery
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppSDelivery/AppSDelivery/AppSDelivery/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppSDelivery.Models;
+using AppSDelivery.ViewModels;
+
+namespace AppSDelivery.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}

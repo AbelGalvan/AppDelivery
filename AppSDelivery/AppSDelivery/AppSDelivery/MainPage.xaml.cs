ae504f85d16aac5e2acd17cfaf54812d46998f68
AbelGalvan <37642705+AbelGalvan@users.noreply.github.com>
2020-08-13 23:43:34 -0600

    Blank

---
+++ AppSDelivery/AppSDelivery/AppSDelivery/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppSDelivery
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
